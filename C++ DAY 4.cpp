8cb4444391002fd6f67763f4820f55e5e265c13c
Khaled M <202011137@gmail.edu.sa>
2022-07-27 14:22:06 +0300

    new

--- C++ DAY 4.cpp
+++ C++ DAY 4.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#import <string>
+using namespace std;
+int main(){
+     //for(int i =0; i <  4; i++){
+         //cout<< i <<"\n";
+//}
+int i=1;
+    while (i<=3& i>=1)
+    {cout<< "Hello World, i:"<< i<< endl;
+i++;
+}
     return 0;
 }
