6c7b9e859c49c871a26f66b4788bb0e5cfda2900
Khaled M <202011137@gmail.edu.sa>
2022-07-27 12:23:50 +0300

    Initial commit

---
+++ Khaled c++ ex.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <math.h>
+#import <string>
+using namespace std;
+int main() {
+    //DAY 2 PRACTICES
+
+
+    //int dob_y;
+    //time_t now = time(0);
+    //tm *ltm = localtime(&now);
+    //int yyy = 1900 + ltm->tm_year;
+    //cout << "The current Year is:" << yyy << endl;
+    //cout << "Please enter your dob as Year";
+    //cin >> dob_y;
+    //cout << yyy - dob_y << " is your year braith day";
+
+    //DAY 3 PRACTICES
+
+
+    //int result= (4+5)*6+2;
+    //cout<< result;
+
+    //int a= 5+7%2;
+    //cout<< a;
+    //int x = 10 , y;
+    //y= x++;
+    //cout<< x;
+    //int x= 10,y;
+    //y=++x+x++ +x;
+    //cout<<y;
+    //int x= 10 ;
+    //cout<<sqrt(64) <<endl;
+    //cout<<round(2.4)<<endl;\
+    //cout<<log
+
+
+    //string firstname = "khaled";
+    //string lastname = " mohammed";
+    //string fullname = firstname.append(lastname);
+    //cout<< fullname;
+
+    // string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    // cout<< "The length of txt string is:" <<txt.length();
+
+    //string myString = "Hello";
+    //myString[0] = 'J';
+    //cout<< myString;
+    //int num1, num2;
+    //int sum, sub, mult, mod;
+    //float div;
+    //cout<< "Enter first number:";
+    //cin>> num1;
+    //cout<< "Enter second number:";
+    //cin>> num2;
+    //sum= num1+num2;
+    //sub= num1-num2;
+    //mult= num1*num2;
+    //mod= num1%num2;
+    //div= (float)num1/ num2;
+    //cout<< "sum is ="<< sum <<endl;
+    //cout<< "sub is ="<< sub <<endl;
+    //cout<< "milt is ="<< mult <<endl;
+    //cout<< "mod is ="<< mod <<endl;
+//string Name;
+//cout<< "enter your name";
+//cin>> Name;
+//if (Name=="Ahmad")
+//{if (Name=="Ahmad")
+//cout<< " Welcome Ahmad ";
+//}
+    //cout <<"Hi:" <<Name<< endl;
+    //string Username;
+    //cout<< "Enter Your Username:";
+    //cin>> Username;
+
+    //int mon=12;
+//switch(mon)
+//{
+//case 1:
+//cout<< "January"
+//break;
+//case 2:
+//cout<< "Febraury"
+//break;
+
+//case 3:
+//cout<< "Mrch"
+//break;
+
+//case 4:
+//cout<< "April"
+//break;
+    //  default:
+
+//int a[]= {82, 95, 53, 74, 53, 37, 86,};
+//cout<< a[5];
+    //int A[2][2] ={(22,23,33),(15,25,43),(23,56,43)};
+    //cout<< A[1][0];
+
+
+
+   // string name;
+//getline(cin, name);
+//cout<< name;
+
+int num = 25;
+int *iptr;
+iptr= &num;
+cout<<iptr<<endl;
+cout<<*iptr;
+
+return 0;
+
+}
