4996e59d0cc41da253cbfadd554458da1cdb8379
Khaled M <202011137@gmail.edu.sa>
2022-07-28 12:55:20 +0300

    new

--- C++ DAY 4.cpp
+++ C++ DAY 4.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #import <string>
 using namespace std;
-int main(){
-     //for(int i =0; i <  4; i++){
-         //cout<< i <<"\n";
-//}
-int i=1;
-    while (i<=3& i>=1)
-    {cout<< "Hello World, i:"<< i<< endl;
-i++;
+int myFunction(int x, int y) {
+    return y + x;
 }
+int main() {
+    //for(int i =0; i <  4; i++){
+    //cout<< i <<"\n";
+//}
+//int i=1;
+    //while (i<=3& i>=1)
+    //{cout<< "Hello World, i:"<< i<< endl;
+//i++;
+    cout << myFunction(5,3);
     return 0;
 }
